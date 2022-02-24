c071704aeb832663dc2825b510209ae07bb6f4d0
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-24 17:19:21 +0530

    Solving using Switch Case Statement

--- src/day10EmpWage/EmpWageBuilder.java
+++ src/day10EmpWage/EmpWageBuilder.java
@@ -1,41 +1,40 @@
 package day10EmpWage;
 
 /*
- * Add Part time Employee & Wage
+ * Solving using Switch Case Statement
  */
 public class EmpWageBuilder {
 
 	// constants
-	public final static int IS_FULL_TIME = 1;
-	public final static int EMP_RATE_PER_HR = 20;
-	public final static int IS_PART_TIME = 2;
+	final static int IS_FULL_TIME = 1;
+	final static int EMP_RATE_PER_HR = 20;
+	final static int IS_PART_TIME = 2;
 
 	public static void main(String args[]) {
-
 		// welcome message
 		System.out.println("Welcome to employee wage computation problem");
 
 		// variables
 		double empCheck;
 		int empHrs = 0;
-		int empWage = 0; // initial wage
+		int empWage = 0;
 
-		/*
-		 * random function to get random numbers
-		 */
+		// computing employee daily working hrs by random method
 		empCheck = Math.floor(Math.random() * 10) % 3;
 
-		// checking how many hrs employee has worked
-		if (empCheck == IS_FULL_TIME)
+		switch ((int) empCheck) {
+		case IS_FULL_TIME:
 			empHrs = 8;
+			break;
 
-		else if (empCheck == IS_PART_TIME)
+		case IS_PART_TIME:
 			empHrs = 4;
+			break;
 
-		else
+		default:
 			empHrs = 0;
-
+		}
 		empWage = empHrs * EMP_RATE_PER_HR;
-		System.out.println("EmpWage = " + empWage);
+		System.out.println("EmpWage for a day " + empWage);
 	}
 }
