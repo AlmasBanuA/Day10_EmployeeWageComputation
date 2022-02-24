ee4f6d8856d02116652ab2c1b7f5ad217dc8f967
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-24 17:12:09 +0530

    Adding Part Time Employee and Wage

--- src/day10EmpWage/EmpWageBuilder.java
+++ src/day10EmpWage/EmpWageBuilder.java
@@ -1,17 +1,19 @@
 package day10EmpWage;
 
 /*
- * Calculating Daily Employee Wage
+ * Add Part time Employee & Wage
  */
 public class EmpWageBuilder {
+
+	// constants
+	public final static int IS_FULL_TIME = 1;
+	public final static int EMP_RATE_PER_HR = 20;
+	public final static int IS_PART_TIME = 2;
+
 	public static void main(String args[]) {
 
 		// welcome message
 		System.out.println("Welcome to employee wage computation problem");
-
-		// constants
-		final int IS_FULL_TIME = 1;
-		final int EMP_RATE_PER_HR = 20;
 
 		// variables
 		double empCheck;
@@ -21,12 +23,18 @@ public class EmpWageBuilder {
 		/*
 		 * random function to get random numbers
 		 */
-		empCheck = Math.floor(Math.random() * 10) % 2;
+		empCheck = Math.floor(Math.random() * 10) % 3;
+
 		// checking how many hrs employee has worked
 		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
+
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+
 		else
 			empHrs = 0;
+
 		empWage = empHrs * EMP_RATE_PER_HR;
 		System.out.println("EmpWage = " + empWage);
 	}
