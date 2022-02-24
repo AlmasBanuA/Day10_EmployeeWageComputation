da2e107b777b9b92fea7af8b75c0ce9e3f81d0cc
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-24 18:18:31 +0530

    Save the total wage for each company

--- src/day10EmpWage/EmpWageBuilder.java
+++ src/day10EmpWage/EmpWageBuilder.java
@@ -10,28 +10,44 @@ public class EmpWageBuilder {
 	final static int IS_FULL_TIME = 1;
 	final static int IS_PART_TIME = 2;
 
+	// instance variable
+	private final String company;
+	private final int numbersOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private final int empRatePerHr;
+
+	public EmpWageBuilder(String company, int numbersOfWorkingDays, int maxHoursPerMonth, int empRatePerHr) {
+		super();
+		this.company = company;
+		this.numbersOfWorkingDays = numbersOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+		this.empRatePerHr = empRatePerHr;
+	}
+
 	// method to get working hour of a employee for a day
-	public static int getEmpHrs() {
+	public int getEmpHrs() {
 		int empHrs;
 		double empCheck = Math.floor(Math.random() * 10) % 3;
 		switch ((int) empCheck) {
 		case IS_FULL_TIME:
 			empHrs = 8;
 			break;
-			
+
 		case IS_PART_TIME:
 			empHrs = 4;
 			break;
-			
+
 		default:
 			empHrs = 0;
 		}
 		return empHrs;
 	}
 
-	/* method to calculate total salary till the maximum days or maximum hrs  is reached*/
-	public static void empWgaeComputation(String company, int numbersOfWorkingDays, int maxHoursPerMonth,
-			int empRatePerHr) {
+	/*
+	 * method to calculate total salary till the maximum days or maximum hrs is
+	 * reached
+	 */
+	public void empWgaeComputation() {
 		int days = 0;
 		int totalWorkingHrs = 0, totalEmpWage;
 		while (days < numbersOfWorkingDays && totalWorkingHrs <= maxHoursPerMonth) {
@@ -41,13 +57,15 @@ public class EmpWageBuilder {
 			System.out.println("emp hrs : " + empHrs);
 		}
 		totalEmpWage = totalWorkingHrs * empRatePerHr;
-		System.out.println("Total Emp wage for company " + company + " is " + totalEmpWage);
+		System.out.println("Total Emp wage for company " + this.company + " is " + totalEmpWage);
 	}
 
 	public static void main(String args[]) {
 		// welcome message
 		System.out.println("Welcome to employee wage computation problem");
-		empWgaeComputation("Dmart", 20, 10, 2);
-		empWgaeComputation("Reliance", 10, 20, 4);
+		EmpWageBuilder dMart = new EmpWageBuilder("Dmart", 20, 10, 2);
+		EmpWageBuilder reliance = new EmpWageBuilder("Reliance", 10, 20, 4);
+		dMart.empWgaeComputation();
+		reliance.empWgaeComputation();
 	}
 }
