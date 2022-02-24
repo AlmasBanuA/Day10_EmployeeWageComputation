0e89edbb31932675c874fbed67871d3dfa399d06
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-24 17:01:34 +0530

    Calculating daily Employee Wage

--- src/day10EmpWage/EmpWageBuilder.java
+++ src/day10EmpWage/EmpWageBuilder.java
@@ -1,28 +1,33 @@
 package day10EmpWage;
 
 /*
- * Checking Employee is Present or Absent
+ * Calculating Daily Employee Wage
  */
 public class EmpWageBuilder {
-
 	public static void main(String args[]) {
-		
+
 		// welcome message
 		System.out.println("Welcome to employee wage computation problem");
-		
+
 		// constants
 		final int IS_FULL_TIME = 1;
-		
+		final int EMP_RATE_PER_HR = 20;
+
 		// variables
 		double empCheck;
-		
+		int empHrs = 0;
+		int empWage = 0; // initial wage
+
 		/*
 		 * random function to get random numbers
 		 */
 		empCheck = Math.floor(Math.random() * 10) % 2;
+		// checking how many hrs employee has worked
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+			empHrs = 8;
 		else
-			System.out.println("Employee is Absent");
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HR;
+		System.out.println("EmpWage = " + empWage);
 	}
 }
